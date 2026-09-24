e7d665b09acbd1d4b23b689b811293f7d9768d7f
RamalheiraJV <ramalheira.joao@escola.pr.gov.br>
2026-09-24 11:35:41 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,26 +4,34 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento."
+                afirmacao: ["Você é uma pessoa reflexiva e busca aprendizado e conhecimento.",
+                afirmacao: "Você é uma pessoa reflexiva, que valoriza a busca constante por aprendizado e conhecimento."
+                ]
             },
             {
                 texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos."
+                afirmacao:[ "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos.",
+                afirmacao: "Você demonstra ser uma pessoa observadora e atenta, preocupando-se com o bem-estar de seus alunos."
+                ]
             }    
            
         ]
     },
-    {
+    {                                                                                                                                                           
        
             enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
             alternativas: [
                 {
                     texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
+                    afirmacao:[ "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores.",
+                    afirmacao: "Você apresenta um perfil de pesquisador e inovador, buscando, por meio da inovação, soluções voltadas à preservação do meio ambiente e à sustentabilidade para as futuras gerações."
+                ]
                 },
                 {
                     texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
-                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
+                    afirmacao:[ "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico.",
+                    afirmacao: "Você demonstra valorizar a utilização consciente e prioritária dos recursos disponíveis, considerando a preservação do meio ambiente sem deixar de levar em conta os aspectos econômicos."
+                    ]
                 }    
                
             ]
@@ -34,12 +42,16 @@ const perguntas = [
             alternativas: [
                 {
                     texto: "Na maior parte do tempo, com certeza impacta, pois é a partir da autoestima que temos um olhar positivo ou negativo sobre nossas ações diárias. Sempre estou em busca de mais autoconhecimento.",
-                    afirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo."
+                    afirmacao: ["Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo.",
+                    afirmacao: "Você reconhece a importância de desenvolver uma boa autoestima e busca aprofundar seus conhecimentos sobre o tema, procurando enxergar a vida de maneira positiva."
+                    ]
                 },
                 {
                     texto:    "Apenas ocasionalmente ou raramente. Às vezes minha autoestima oscila e isso pode afetar negativamente minha qualidade de vida em certas ocasiões.",
                
-                    afirmacao: "Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional."
+                    afirmacao: ["Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional.",
+                    afirmacao: "Você demonstra a necessidade de ampliar o autoconhecimento e revela uma consciência crescente sobre a importância de cuidar da própria percepção, do equilíbrio e do bem-estar emocional."
+                    ]
                 }    
                
             ]
